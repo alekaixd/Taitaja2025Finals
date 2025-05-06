a496eb0ec76d1fbf49511794b9de40216fc09834
MrPekoni <Lukaspietila37@gmail.com>
2025-05-06 13:41:09 +0300

    Added Flavour

--- Wok/Assets/CardClass.cs
+++ Wok/Assets/CardClass.cs
@@ -1,16 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardClass : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string CardName;
+    public string CardDescription;
+    public CardFlavor Flavor;
+    public CardFlavor SecondaryFlavor;
+    public List<CardFlavor> FlavorList = new(); // Additional flavors
 }
--- Wok/Assets/GameManager.cs
+++ Wok/Assets/GameManager.cs
@@ -1,17 +1,21 @@
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Vector2 MousePosition;
     public GameObject CardPrefab;
     public List<CardClass> Deck = new();
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    
     void AddCardToDeck()
     {
         // Adds a card to the deck
@@ -22,3 +26,34 @@ public class GameManager : MonoBehaviour
         MousePosition = Mouse.current.position.ReadValue();
     }
 }
+    public enum CardFlavor
+    {
+        Spicy = 0,
+        Sweet = 1,
+        Sour = 2,
+        Savoury = 3,
+        Salty = 4,
+    }
+
+    public class FlavourClass
+    {
+        CardFlavor Flavor;
+        int FlavourValue;
+        public FlavourClass(CardFlavor flavor, int flavourValue)
+        {
+            Flavor = flavor;
+            FlavourValue = flavourValue;
+        }
+    }
+    public class FlavourBuffClass
+    {
+        CardFlavor Flavor;
+        int FlavourValue;
+        int RoundDuration;
+        public FlavourBuffClass(CardFlavor flavor, int flavourValue, int roundDuration)
+        {
+            Flavor = flavor;
+            FlavourValue = flavourValue;
+            RoundDuration = roundDuration;
+        }
+    }
